863976032c3ff2bc97800a2ab1a1b5474e1ed7a3
Salah Eddine Hali <salhali@e2r5p2.1337.ma>
2025-05-19 19:19:43 +0100

    nothing

---
+++ builtins/builtins.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtins.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/19 17:09:02 by salhali           #+#    #+#             */
+/*   Updated: 2025/05/19 19:19:31 by salhali          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../minishell.h"
+
+int is_builtin(char *cmd)
+{
+    if (!cmd)
+        return (0);
+    if (ft_strcmp(cmd, "echo") == 0 ||
+        ft_strcmp(cmd, "cd") == 0 ||
+        ft_strcmp(cmd, "pwd") == 0 ||
+        ft_strcmp(cmd, "export") == 0 ||
+        ft_strcmp(cmd, "unset") == 0 ||
+        ft_strcmp(cmd, "env") == 0 ||
+        ft_strcmp(cmd, "exit") == 0)
+        return (1);
+    return (0);
+}
+
+
+// int execute_builtin(t_command *cmd, t_shell *shell)
+// {
+//     if (ft_strcmp(cmd->cmd, "echo") == 0)
+//         return (builtin_echo(cmd));
+//     else if (ft_strcmp(cmd->cmd, "cd") == 0)
+//         return (builtin_cd(cmd, shell));
+//     else if (ft_strcmp(cmd->cmd, "pwd") == 0)
+//         return (builtin_pwd());
+//     else if (ft_strcmp(cmd->cmd, "export") == 0)
+//         return (builtin_export(cmd, shell));
+//     else if (ft_strcmp(cmd->cmd, "unset") == 0)
+//         return (builtin_unset(cmd, shell));
+//     else if (ft_strcmp(cmd->cmd, "env") == 0)
+//         return (builtin_env(shell));
+//     else if (ft_strcmp(cmd->cmd, "exit") == 0)
+//         return (builtin_exit(cmd, shell));
+//     return (1); // Error
+// }
