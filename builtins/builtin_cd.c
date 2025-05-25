e9b61860b97d52c5bd513ae8cbff736f42e38912
Salah Eddine Hali <salhali@e2r5p1.1337.ma>
2025-05-25 12:07:11 +0100

    l.modification: check builtins X %)

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -1,18 +1,53 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   cd.c                                               :+:      :+:    :+:   */
+/*   builtin_cd.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 19:45:22 by salhali           #+#    #+#             */
-/*   Updated: 2025/05/24 21:45:57 by salah            ###   ########.fr       */
+/*   Updated: 2025/05/25 12:06:44 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "mini.h"
+
+#include "../minishell.h"
 
 int builtin_cd(c_cmd *cmd, t_shell *shell)
 {
-  
+    char *path;
+    char current_dir[1024];
+
+    // Save current directory for OLDPWD
+    if (getcwd(current_dir, 1024) == NULL)
+        return (1);
+
+    // If no argument, go to HOME
+    if (!cmd->array || !cmd->array[1])
+        path = get_env_value(shell->env, "HOME");
+    else if (ft_strcmp(cmd->array[1], "~") == 0)
+        path = get_env_value(shell->env, "HOME");
+    else
+        path = cmd->array[1];
+
+    if (!path)
+    {
+        ft_putstr_fd("cd: HOME not set\n", 2);
+        return (1);
+    }
+
+    if (chdir(path) == -1)
+    {
+        ft_putstr_fd("cd: ", 2);
+        ft_putstr_fd(path, 2);
+        ft_putstr_fd(": No such file or directory\n", 2);
+        return (1);
+    }
+
+    // Update PWD and OLDPWD environment variables
+    update_env_variable(shell, "OLDPWD", current_dir);
+    if (getcwd(current_dir, 1024) != NULL)
+        update_env_variable(shell, "PWD", current_dir);
+
+    return (0);
 }
