5ad294d220cd7791990e325e2c9863bdf0210c4c
Salah Eddine Hali <salhali@e2r5p3.1337.ma>
2025-05-29 20:47:47 +0100

    last modification (cd) check export

--- builtins/builtin_cd.c
+++ builtins/builtin_cd.c
@@ -6,48 +6,63 @@
 /*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/18 19:45:22 by salhali           #+#    #+#             */
-/*   Updated: 2025/05/28 20:46:10 by salhali          ###   ########.fr       */
+/*   Updated: 2025/05/28 22:54:55 by salhali          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../minishell.h"
 
-int builtin_cd(c_cmd *cmd, t_shell *shell)
-{
-    char *path;
-    char current_dir[1024];
+// int builtin_cd(c_cmd *cmd, t_shell *shell)
+// {
+//     char *path;
+//     char current_dir[1024];
 
-    // Get the name of the current working directory
-    if (getcwd(current_dir, 1024) == NULL)
-        return (1);
+//     // Get the name of the current working directory
+//     if (getcwd(current_dir, 1024) == NULL)
+//         return (1);
+//     //check 
 
+//     if(find_path(cmd->array[1]));
+    
+//     int len = 0;
+//     while(cmd->array[len])
+//         len++;
+//     if (len <= 2)
+//     {
+//          // printf("cmd->flags = %d\n", cmd->qflag);
+//         if (cmd->array[1] == NULL)
+//             path = get_env_value(shell->env, "HOME"); // ou hta ila makan walo nsift lhome 
+//         else if (ft_strcmp(cmd->array[1], "~") == 0)
+//             path = get_env_value(shell->env, "HOME"); // ila kan cd  ~ nsiftk lhome nichan 
+//         else
+//             path = cmd->array[1]; // 3tini hadak path ndik lih
+//         if (path == NULL)
+//         {
+//             ft_putstr_fd("cd: HOME not set\n", 2);
+//             return (1);
+//         }
 
-    // if (cmd->array[1] == NULL)
-    //     path = get_env_value(shell->env, "HOME"); // ou hta ila makan walo nsift lhome 
-    // else if (ft_strcmp(cmd->array[1], "~") == 0)
-    //     path = get_env_value(shell->env, "HOME"); // ila kan cd  ~ nsiftk lhome nichan 
-    // else
-    path = cmd->array[1]; // 3tini hadak path ndik lih
-    if (path == NULL)
-    {
-        ft_putstr_fd("cd: HOME not set\n", 2);
-        return (1);
-    }
+//         if (chdir(path) == -1)
+//         {
+//             ft_putstr_fd("cd: ", 2);
+//             ft_putstr_fd(path, 2);
+//             ft_putstr_fd("No such file or directory\n", 2);
+//             return (1);
+//         }
 
-    if (chdir(path) == -1)
-    {
-        ft_putstr_fd("cd: ", 2);
-        ft_putstr_fd(path, 2);
-        ft_putstr_fd(": No such file or directory\n", 2);
-        return (1);
-    }
-
-    // Update PWD and OLDPWD environment variables
-    update_env_variable(shell, "OLDPWD", current_dir);
-    if (getcwd(current_dir, 1024) != NULL)
-        update_env_variable(shell, "PWD", current_dir);
-    return (0);
-}
+//         // Update PWD and OLDPWD environment variables
+//         update_env_variable(shell, "OLDPWD", current_dir);
+//         if (getcwd(current_dir, 1024) != NULL)
+//             update_env_variable(shell, "PWD", current_dir);
+//     }
+//     else
+//     {
+//             ft_putstr_fd("cd: ", 2);
+//             ft_putstr_fd(": too many arguments\n", 2);
+//             return (1);
+//     }
+//     return (0);
+// }
 
 // int builtin_cd(c_cmd *cmd, t_shell *shell)
 // {
@@ -90,3 +105,51 @@ int builtin_cd(c_cmd *cmd, t_shell *shell)
 //         pdate_env_variable(shell, "PWD", current_dir);
 //     return (0);
 // }
+
+
+// #include "../minishell.h"
+
+int builtin_cd(c_cmd *cmd, t_shell *shell)
+{
+    char *path;
+    char current_dir[PATH_MAX];
+    int i;
+
+    //get current dir
+    if (getcwd(current_dir, sizeof(current_dir)) == NULL)
+    {
+        perror("cd: getcwd");
+        return (1);
+    }
+
+    i = 0;
+    while(cmd->array[i])
+        i++;
+    if (i > 2)
+        return(ft_putstr_fd("bash: cd: too many arguments\n", 2), 1);
+    if (cmd->array[1] == NULL || ft_strcmp(cmd->array[1], "~") == 0)
+    {
+        path = get_env_value(shell->env, "HOME");
+        if (!path)
+        {
+            ft_putstr_fd("cd: HOME not set\n", 2);
+            return (1);
+        }
+    }
+    else
+        path = cmd->array[1];
+
+    //  chdir to path
+    if (chdir(path) == -1)
+    {
+        ft_putstr_fd("cd: ", 2);
+        perror(path); // better error message
+        return (1);
+    }
+
+    update_env_variable(shell, "OLDPWD", current_dir);
+    if (getcwd(current_dir, sizeof(current_dir)) != NULL)
+        update_env_variable(shell, "PWD", current_dir);
+
+    return (0);
+}
--- builtins/builtin_export.c
+++ builtins/builtin_export.c
@@ -14,39 +14,52 @@
 
 int builtin_export(c_cmd *cmd, t_shell *shell)
 {
-    int i;
-    char *equal_sign;
-    char *name;
-    char *value;
+    // int i;
+    // char *equal_sign;
+    // char *name;
+    // char *value;
+    (void)cmd;
+    char **test;
 
-    if (!cmd->array || !cmd->array[1])
-        return (builtin_env(shell));
 
-    i = 1;
-    while (cmd->array[i] != NULL)
-    {
-        //check wach kayna =
-        equal_sign = ft_strchr(cmd->array[i], '=');
-        // if(equal_sign == NULL)
-        //     printf("spaaam9999 equal_sign == NULL\n");
-        if(equal_sign == NULL)
-            return(0);
-         if (equal_sign != NULL)
-        {
-            //ah kayna so : andir split VAR=ARG {VAR}, {ARG}, {NULL}
-            *equal_sign = '\0';
-            name = cmd->array[i];
-            value = equal_sign + 1;
-            *equal_sign = '='; // Restore original string
-            update_env_variable(shell, name, value);
-        }
-        else
-        {
-            if (get_env_value(shell->env, cmd->array[i]) == NULL)
-                update_env_variable(shell, cmd->array[i], "");
-        }
-        i++;
-    }
+    // shell->env_copy = dup_envp(shell->env);
+    
+    // if (!cmd->array || !cmd->array[1])
+    // {
+    //     // need to sort (bubble sort ); from a z 
+    //     print_env(shell->env_copy);
+    // }
+    test = function_split_env(shell);
+    if(test == NULL)
+        printf("ra salina \n");
+    // i = 1;
+    // while (cmd->array[i] != NULL)
+    // {
+    //     //check wach kayna =
+    //     equal_sign = ft_strchr(cmd->array[i], '=');
+    //     // if(equal_sign == NULL)
+    //     //     printf("spaaam9999 equal_sign == NULL\n");
+    //     // if(equal_sign == NULL)
+    //     //     return(0);
+    //      if (equal_sign != NULL)
+    //     {
+    //         //ah kayna so : andir split VAR=ARG {VAR}, {ARG}, {NULL}
+    //         *equal_sign = '\0';
+    //         name = cmd->array[i];
+    //         value = equal_sign + 1;
+    //                     *equal_sign = '='; // Restore original string
+
+    //         update_env_variable(shell, name, value);
+    //         // update_env_variable(shell, name, value);
+    //     }
+    //     else
+    //     {
+    //         // export a="" 
+    //         if (get_env_value(shell->env_copy, cmd->array[i]) == NULL)
+    //             update_env_variable(shell, cmd->array[i], "");
+    //     }
+    //     i++;
+    // }
 
     return (0);
 }
