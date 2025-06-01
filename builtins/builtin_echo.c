56ef234966204c56f17ca1473727afc0d242156e
salaheddine <salaheddineh7@gmail.com>
2025-06-01 16:27:39 +0100

    check (echo) OK

--- builtins/builtin_echo.c
+++ builtins/builtin_echo.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_echo.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: salhali <salhali@student.42.fr>            +#+  +:+       +#+        */
+/*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 19:21:22 by salhali           #+#    #+#             */
-/*   Updated: 2025/05/31 19:12:50 by salhali          ###   ########.fr       */
+/*   Updated: 2025/06/01 16:27:03 by salah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,6 @@
 
 //     i = 1;
 //     n_option = 0;
-//     // printf("array fih : %s\n", cmd->array[0]);
 //     if (cmd->array && cmd->array[i] && ft_strcmp_echo(cmd->array[i], "-n") == 0)
 //     {
 //         n_option = 1;
@@ -32,7 +31,7 @@
 //     }
 //     if (!n_option)
 //         ft_putchar_fd('\n', 1);
-    
+
 //     return (0);
 // }
 
@@ -59,15 +58,16 @@ int builtin_echo(c_cmd *cmd)
 
     i = 1;
     n_option = 0;
-    
-    // Handle multiple -n flags (like bash does)
-    while (cmd->array && cmd->array[i] && ft_strcmp_echo(cmd->array[i], "-n") == 0)
+
+    if (!cmd || !cmd->array)
+        return (0);
+
+    while (cmd->array[i] && ft_strcmp_echo(cmd->array[i], "-n") == 0)
     {
         n_option = 1;
         i++;
     }
-    
-    // Print arguments with spaces between them
+
     while (cmd->array[i])
     {
         ft_putstr_fd(cmd->array[i], 1);
@@ -75,33 +75,30 @@ int builtin_echo(c_cmd *cmd)
             ft_putchar_fd(' ', 1);
         i++;
     }
-    
-    // Add newline unless -n option was used
-    if (!n_option)
+    if (n_option == 0)
         ft_putchar_fd('\n', 1);
-    
+
     return (0);
 }
 
-// Fixed string comparison function
 int ft_strcmp_echo(char *s1, const char *s2)
 {
     int i;
 
     if (!s1 || !s2)
         return (1);
-    
+
     i = 0;
     while (s1[i] && s2[i])
     {
-        if (s1[i] != s2[i])
-            return (1);
+        if (s1[i] == s2[i])
+            return (0);
         i++;
     }
-    
+
     // Both strings must end at the same time for exact match
     if (s1[i] == '\0' && s2[i] == '\0')
         return (0);
-    
+
     return (1);
 }
