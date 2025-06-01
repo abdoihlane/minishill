ff4176676c1a24a7435fba2e39075ec58ac4aeb6
salaheddine <salaheddineh7@gmail.com>
2025-06-01 16:49:23 +0100

    echo( OK )

--- builtins/builtin_echo.c
+++ builtins/builtin_echo.c
@@ -6,63 +6,21 @@
 /*   By: salah <salah@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/19 19:21:22 by salhali           #+#    #+#             */
-/*   Updated: 2025/06/01 16:27:03 by salah            ###   ########.fr       */
+/*   Updated: 2025/06/01 16:44:00 by salah            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
-// int builtin_echo(c_cmd *cmd)
-// {
-//     int i;
-//     int n_option;
-
-//     i = 1;
-//     n_option = 0;
-//     if (cmd->array && cmd->array[i] && ft_strcmp_echo(cmd->array[i], "-n") == 0)
-//     {
-//         n_option = 1;
-//         i++;
-//     }
-//     while (cmd->array[i])
-//     {
-//         ft_putstr_fd(cmd->array[i], 1);
-//         if (cmd->array[i + 1])
-//             ft_putchar_fd(' ', 1);
-//         i++;
-//     }
-//     if (!n_option)
-//         ft_putchar_fd('\n', 1);
-
-//     return (0);
-// }
-
-// int    ft_strcmp_echo(char *s1, const char *s2)
-// {
-//     int i;
-
-//     i = 0;
-//     while(s1[i])
-//     {
-//         if(s1[i] == s2[i])
-//             return(0);
-//         i++;
-//     }
-//     return(1);
-// }
 
 #include "../minishell.h"
 
 int builtin_echo(c_cmd *cmd)
 {
-    int i;
-    int n_option;
-
-    i = 1;
-    n_option = 0;
+    int i = 1;
+    int n_option = 0;
 
     if (!cmd || !cmd->array)
         return (0);
 
-    while (cmd->array[i] && ft_strcmp_echo(cmd->array[i], "-n") == 0)
+    while (cmd->array[i] && ft_strcmp_echo(cmd->array[i]) == 0)
     {
         n_option = 1;
         i++;
@@ -75,30 +33,25 @@ int builtin_echo(c_cmd *cmd)
             ft_putchar_fd(' ', 1);
         i++;
     }
-    if (n_option == 0)
+
+    if (!n_option)
         ft_putchar_fd('\n', 1);
 
     return (0);
 }
-
-int ft_strcmp_echo(char *s1, const char *s2)
+int ft_strcmp_echo(const char *s)
 {
-    int i;
+    int i = 0;
 
-    if (!s1 || !s2)
+    if (s[0] != '-')
         return (1);
 
-    i = 0;
-    while (s1[i] && s2[i])
-    {
-        if (s1[i] == s2[i])
-            return (0);
+    i = 1;
+    while (s[i] == 'n')
         i++;
-    }
 
-    // Both strings must end at the same time for exact match
-    if (s1[i] == '\0' && s2[i] == '\0')
-        return (0);
+    if (s[i] != '\0')
+        return (1);
 
-    return (1);
+    return (0);
 }
