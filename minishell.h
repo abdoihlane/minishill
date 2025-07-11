8aad52ceff2f23d38ef696dbbfda48200dbf9388
Salah Eddine Hali <salhali@c3r1p2.1337.ma>
2025-07-11 18:20:39 +0100

    last version ...

--- minishell.h
+++ minishell.h
@@ -143,7 +143,7 @@ int                 builtin_echo(c_cmd *cmd);                     // Echo comman
 int                 builtin_cd(c_cmd *cmd, t_shell *shell);       // Change directory
 int                 builtin_pwd(void);                            // Print working directory
 int                 builtin_export(c_cmd *cmd, t_shell *shell);   // Export env variables
-int builtin_env(t_shell *shell);
+int                 builtin_env(t_shell *shell);
 int                 builtin_unset(c_cmd *cmd, t_shell *shell);    // Unset env variables
 //                  function utils check mul
 int                 is_numeric(const char *str);
